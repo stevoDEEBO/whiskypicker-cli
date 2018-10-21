dd0c020dab7f0b9c4fbf5f2aaa6004207e88fea7
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-10-21 19:27:58 +0000

    created scraper for index page (to create list of whiskies)

---
+++ lib/whiskypicker/cli/whisky.rb
@@ -0,0 +1,24 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+class WhiskyPicker::Whisky
+  attr_accessor :name, :profile_url, :country, :region_type, :proof, :rating, :description
+
+  #scrape whisky type index page for name and url of whiskies
+  def self.scrape_index_page(index_url)
+    whiskies = []
+    doc = Nokogiri::HTML(open(index_url))
+
+    whisky_time = doc.css(".item")
+    whisky_time.each do |whisky|
+      my_whisky = self.new
+      my_whisky.name = whisky.css("a").attribute("title").text.strip
+      my_whisky.profile_url = whisky.css("a").attribute("href").value
+      whiskies << my_whisky
+    end
+    whiskies
+
+  end
+
+end
